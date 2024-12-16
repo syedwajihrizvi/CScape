05d6ec82dbfac3069d178491c2dd458bfb078ddc
AB Rizvi <abrizvi@mac.home>
2024-12-15 22:57:13 -0500

    Update apiClient for production

--- src/utils/clients/apiClient.ts
+++ src/utils/clients/apiClient.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const url = "http://localhost:3000/cscape/api/"
+const url = import.meta.env.PROD ? 
+        'https://young-beyond-42845-2867f1d39669.herokuapp.com/cscape/api' : 
+        'http://localhost:3000/cscape/api/'
 
 export const apiClient = axios.create({
     baseURL: url
