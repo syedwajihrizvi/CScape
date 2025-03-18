0c9b8833a132b3f67432d65a1d4557f0f7cc0464
AB Rizvi <abrizvi@ABs-MacBook-Pro.local>
2025-03-17 23:52:29 -0400

    Added temp debugging

--- src/components/map/DisplayMap.tsx
+++ src/components/map/DisplayMap.tsx
@@ -25,7 +25,7 @@ function DisplayMap() {
         navigate('/main')
         setSelectedPlace(place)
     }
-    console.log("Google Maps API Key:", import.meta.env.VITE_GOOGLE_MAPS_API_KEY ? "Loaded" : "Not Found");
+    console.log("Google Maps API Key:", import.meta.env.VITE_GOOGLE_MAPS_API_KEY);
     const renderHandler = () => {
         if (selectedPlan == null || !tripInfo)
             return <MapHandler place={selectedPlace} marker={marker} 
